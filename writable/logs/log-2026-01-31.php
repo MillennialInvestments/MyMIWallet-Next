bc00d41e7428cf948f7fc5698830b6b12f836808
MillennialInvestments <admin@mymimillennialinvestments.com>
2026-01-31 12:00:46 -0800

    01/31/2026 - Writable/logs - TBJ

--- writable/logs/log-2026-01-31.php
+++ writable/logs/log-2026-01-31.php
@@ -173,3 +173,12 @@ in SYSTEMPATH/Config/BaseConfig.php on line 82.
  4 SYSTEMPATH/Boot.php(337): CodeIgniter\Cache\FactoriesCache->load('config')
  5 SYSTEMPATH/Boot.php(62): CodeIgniter\Boot::loadConfigCache()
  6 FCPATH/index.php(68): CodeIgniter\Boot::bootWeb(Object(Config\Paths))
+CRITICAL - 2026-01-31 12:00:02 --> TypeError: Cannot assign null to property Config\App::$enableKint of type bool
+[Method: GET, Route: API/Management/Run-CRON-Tasks]
+in SYSTEMPATH/Config/BaseConfig.php on line 82.
+ 1 ROOTPATH/writable/cache/FactoriesCache_config(55): CodeIgniter\Config\BaseConfig::__set_state([...])
+ 2 SYSTEMPATH/Cache/FactoriesCache/FileVarExportHandler.php(38): include('/home/mymiteam/mymiwallet/site/current/writable/cache/FactoriesCache_config')
+ 3 SYSTEMPATH/Cache/FactoriesCache.php(48): CodeIgniter\Cache\FactoriesCache\FileVarExportHandler->get('FactoriesCache_config')
+ 4 SYSTEMPATH/Boot.php(337): CodeIgniter\Cache\FactoriesCache->load('config')
+ 5 SYSTEMPATH/Boot.php(62): CodeIgniter\Boot::loadConfigCache()
+ 6 FCPATH/index.php(68): CodeIgniter\Boot::bootWeb(Object(Config\Paths))
