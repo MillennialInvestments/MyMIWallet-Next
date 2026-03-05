c6777b84019dc8fd384ad24bee62970710779c1f
MillennialInvestments <admin@mymimillennialinvestments.com>
2026-03-04 20:53:33 -0800

    03/04/2026 - Logs, docs, writable/logs - TBJ

---
+++ writable/logs/log-2026-03-05.php
@@ -0,0 +1,22 @@
+<?php defined('SYSTEMPATH') || exit('No direct script access allowed'); ?>
+
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [REQUEST][START] GET /
+DEBUG - 2026-03-05 04:05:53 --> Session: Class initialized using 'CodeIgniter\Session\Handlers\FileHandler' driver.
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [FILTER_BEFORE] /
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [ROUTE] Controller=\App\Controllers\Home Method=index
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [LIFECYCLE][START] GET https://www.mymiwallet.com/index.php/
+DEBUG - 2026-03-05 04:05:53 --> [CSRF] token name=csrf_test_name hash=97c35df941bc49ddd96abe42031ed350
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [INIT] App\Controllers\Home::GET
+INFO - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [MEMORY][controller-start] 6291456
+INFO - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [MEMORY][commonData:start] 6291456
+DEBUG - 2026-03-05 04:05:53 --> MyMIUser L52 - initialized (first log).
+DEBUG - 2026-03-05 04:05:53 --> [MetaService] slug= pageName= cacheHit= path=FALLBACK
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [VIEW_RENDER] themes/public/home
+INFO - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [MEMORY][commonData:start] 10485760
+INFO - 2026-03-05 04:05:53 --> themesMemory usage: 10485760
+DEBUG - 2026-03-05 04:05:53 --> [MetaService] slug= pageName=Home cacheHit= path=DB
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [FILTER_AFTER]
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [LIFECYCLE][END] status=200 duration_ms=718.48 memory_delta=4194304
+INFO - 2026-03-05 04:05:53 --> [REQ_ID=96827b346abe] [PERF] Execution time=0.716372
+DEBUG - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [REQUEST][END]
+INFO - 2026-03-05 04:05:53 --> [REQ_ID=adda812d448b] [PERF] Execution time=0.803566
