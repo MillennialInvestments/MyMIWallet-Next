3e4a58ba2678411d5a24bdd047a8d0fda3529142
MillennialInvestments <admin@mymimillennialinvestments.com>
2026-06-05 21:05:18 -0700

    Register community marketing smoke test command

--- app/Config/Console.php
+++ app/Config/Console.php
@@ -8,6 +8,7 @@ class Console extends BaseConfig
 {
 
     public array $commands = [
+        \App\Commands\Marketing\CommunitiesSmokeTest::class,
         \App\Commands\SolanaPreviewRuntimeSmoke::class,
         \App\Commands\SolanaPreviewBrowserRuntimeSmoke::class,
         \App\Commands\AIOps\API\SchemaValidate::class,
